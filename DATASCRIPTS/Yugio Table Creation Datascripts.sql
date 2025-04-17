0ae94e963c814af05e0a0d639fe42241016851a0
Ubhumma <devonejohnson04@gmail.com>
2025-04-17 18:58:15 -0400

    initialized API, SQL, AND UI

---
+++ DATASCRIPTS/Yugio Table Creation Datascripts.sql
@@ -0,0 +1,41 @@
+
+GO 
+
+USE ytcg-eguideDB;
+
+GO
+
+
+
+CREATE TABLE StarterPack(
+	Id int IDENTITY(1,1) PRIMARY KEY,
+	[Name] NVARCHAR(128) NOT NULL,
+	[Type] NVARCHAR(64) NOT NULL,
+	FrameType NVARCHAR(16),
+	[Description] NVARCHAR(128) NOT NULL,
+	Atk int NOT NULL, 
+	Def int NOT NULL,
+	[Level] int NOT NULL,
+	Race NVARCHAR(32) NOT NULL,
+	Attribute NVARCHAR(32) NOT NULL,
+	ImageURL NVARCHAR(256) 
+);
+
+GO 
+
+CREATE TABLE [User](
+	Id int IDENTITY(1,1) PRIMARY KEY,
+	Username VARCHAR(32) NOT NULL,
+	[Password] VARCHAR(64) NOT NULL,
+	
+	);
+
+GO
+
+CREATE TABLE OwnedCards(
+UserId int FOREIGN KEY REFERENCES [User](Id),
+StarterId int FOREIGN KEY REFERENCES StarterPack(Id),
+IsOwned BIT,
+);
+
+GO 
