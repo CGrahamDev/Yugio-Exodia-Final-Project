5c01c1a521f7b89c4805811ff958f81ca79824d9
Ubhumma <devonejohnson04@gmail.com>
2025-04-17 19:54:01 -0400

    Connection String Hijinks handled and API is functional now

--- ytcg-eGuideAPI/ytcg-eGuideAPI/Program.cs
+++ ytcg-eGuideAPI/ytcg-eGuideAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ytcg_eGuideAPI.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => { options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); });
+
+IConfigurationBuilder buildConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+IConfiguration configuration = buildConfig.Build();
+builder.Services.AddDbContext<YtcgEguiudeDbContext> (options =>
+options.UseSqlServer(configuration.GetConnectionString("ytcg-eguiudeDB"))); // Make sure to use the right name
 
 var app = builder.Build();
 
@@ -21,5 +30,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowAll");
 
 app.Run();
