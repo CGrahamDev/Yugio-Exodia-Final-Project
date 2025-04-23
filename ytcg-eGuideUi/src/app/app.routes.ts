e2c44822c9ea5f10984dddb11a984de0f356693e
Ubhumma <devonejohnson04@gmail.com>
2025-04-22 20:04:34 -0400

    setup routes

--- ytcg-eGuideUi/src/app/app.routes.ts
+++ ytcg-eGuideUi/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HowToPlayComponent } from './how-to-play/how-to-play.component';
+import { OwnedCardsComponent } from './owned-cards/owned-cards.component';
+import { StarterPackComponent } from './starter-pack/starter-pack.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'how-to-play', component:HowToPlayComponent},
+    {path:'owned-cards', component:OwnedCardsComponent},
+    {path:'starter-pack', component:StarterPackComponent},
+    {path:'', redirectTo:'how-to-play', pathMatch:'full'}
+];
